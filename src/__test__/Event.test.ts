27e0ed71f14832eb3a1f7eeae756fa6d76281862
guiquan156 <guiquan156@163.com>
2019-05-11 22:24:32 +0800

    test

--- src/__test__/Event.test.ts
+++ src/__test__/Event.test.ts
@@ -4,10 +4,10 @@ test('data in arguments can set in to instance', () => {
   let event = new Event({
     name: 'kitty',
     school: 'SCNU',
-  });
+  })
   expect(event.name).toBe('kitty');
   expect(event.school).toBe('SCNU');
-});
+})
 
 test('add functions by method add~', () => {
   let event = new Event();
@@ -27,7 +27,7 @@ test('add functions by method add~', () => {
     // say hello
   });
   expect(event.callback.say.hello.__cbs__.length).toBe(1);
-});
+})
 
 test('function added will be exec after emit', () => {
   let event = new Event()
@@ -61,4 +61,45 @@ test('function added will be exec after emit', () => {
   })
   event.emit('say.hello')
   expect(sayHelloCount).toBe(2)
+})
+
+test('the callback will be cleared arfter method off exec', () => {
+  let event = new Event()
+  let sayCount = 0
+  let sayHelloCount = 0
+  let sayWorldCount = 0
+  let sayHelloWorldCount = 0
+
+  event.on('say', function () {
+    ++sayCount
+  })
+  event.on('say.hello', function () {
+    ++sayHelloCount
+  })
+  event.on('say.world', function () {
+    ++sayWorldCount
+  })
+  event.on('say.hello.world', function () {
+    ++sayHelloWorldCount
+  })
+  event.emit('say')
+  event.emit('say.hello')
+  event.emit('say.world')
+  event.emit('say.hello.world')
+
+  expect(sayCount).toBe(1)
+  expect(sayHelloCount).toBe(1)
+  expect(sayWorldCount).toBe(1)
+  expect(sayHelloWorldCount).toBe(1)
+
+  event.off('say.hello')
+  event.emit('say')
+  event.emit('say.hello')
+  event.emit('say.world')
+  event.emit('say.hello.world')
+
+  expect(sayCount).toBe(2)
+  expect(sayHelloCount).toBe(1)
+  expect(sayWorldCount).toBe(2)
+  expect(sayHelloWorldCount).toBe(1)
 })
